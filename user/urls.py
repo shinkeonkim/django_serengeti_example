eac6e091f9367b102c23fa28b795e9c56fc3bbe1
shinkeonkim <singun11@kookmin.ac.kr>
2022-04-24 22:20:32 +0900

    feat: 로그인 View와 Template (/users/signin)을 만들어봅니다.

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import signup
+from .views import signup, signin
 
 app_name = "user"
 
 urlpatterns = [
     path('signup', signup, name="signup"),
+    path('signin', signin, name="signin"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserConfirmationForm
 
 def signup(request):
     form = UserRegistrationForm()    
@@ -15,4 +15,16 @@ def signup(request):
             return redirect("main:index")
 
     return render(request, 'signup.html', { 'form': form })
+
+def signin(request):
+    form = UserConfirmationForm()
+    
+    if request.method == 'POST':
+        form = UserConfirmationForm(request, request.POST)
+
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("main:index")
+
+    return render(request, 'signin.html', { 'form': form })
 
