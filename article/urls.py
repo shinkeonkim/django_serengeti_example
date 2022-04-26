e8b126a4c202b0617ddebde7b6cf6ac33d954be3
shinkeonkim <singun11@kookmin.ac.kr>
2022-04-26 20:37:57 +0900

    feat: Article 수정 View 와 Template (/articles/:id/edit)을 만들어봅니다.

--- article/urls.py
+++ article/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import show, new
+from .views import show, new, edit
 
 app_name = "article"
 
 urlpatterns = [
     path('new', new, name="new"),
+    path('<int:id>/edit', edit, name="edit"),
     path('<int:id>', show, name="show"),
 ]
--- article/views.py
+++ article/views.py
@@ -21,6 +21,26 @@ def new(request):
     return render(request, 'new.html', { 'form': form })
 
 
+def edit(request, id):
+    # 과연 내가 작성한 글만 수정할 수 있는 걸까요?
+    article = get_object_or_404(Article, pk=id)
+    
+    form = ArticleForm(instance=article)
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        
+        if form.is_valid():
+            article = form.save(
+                commit=False
+            )
+            article.save()
+            
+            return redirect('article:show', id=article.id)
+  
+    return render(request, 'edit.html', { 'form': form, 'article': article })
+
+
 def show(request, id):
     article = get_object_or_404(Article, pk=id)
     return render(request, 'show.html', { 'article': article })
