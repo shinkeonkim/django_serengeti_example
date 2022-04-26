6239ca3eb6811278690756e1535e11a8c1f57c36
shinkeonkim <singun11@kookmin.ac.kr>
2022-04-26 20:37:09 +0900

    feat: Article 생성 View 와 Template (/articles/new)을 만들어봅니다.

--- article/urls.py
+++ article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import show
+from .views import show, new
 
 app_name = "article"
 
 urlpatterns = [
+    path('new', new, name="new"),
     path('<int:id>', show, name="show"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from article.models import Article
+from .forms import ArticleForm
+
+def new(request):
+    form = ArticleForm()
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        
+        if form.is_valid():
+            article = form.save(
+                commit=False
+            )
+            article.author = request.user
+            article.save()
+            
+            return redirect('article:show', id=article.id)
+  
+    return render(request, 'new.html', { 'form': form })
+
 
 def show(request, id):
     article = get_object_or_404(Article, pk=id)
