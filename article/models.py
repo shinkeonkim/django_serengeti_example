579748d2378ad04c6297ad4cf3f9cef66d71abdb
shinkeonkim <singun11@kookmin.ac.kr>
2022-04-26 20:33:42 +0900

    feat: Article 모델을 만들어봅니다.

--- article/models.py
+++ article/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    author = models.ForeignKey(
+        'user.User',
+        on_delete=models.CASCADE
+    )  # 작성자
+    title = models.TextField()  # 제목
+    content = models.TextField()  # 내용
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )  # 글 레코드가 생성된 일자
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )  # 글 레코드가 수정된 일자
